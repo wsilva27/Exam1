761f647faa9185fa0a9803b0c77a6e4d92ecfabc
blacksmilez <43358528+blacksmilez@users.noreply.github.com>
2018-10-18 10:32:20 -0400

    Update auth_inc.php

--- php/auth_inc.php
+++ php/auth_inc.php
@@ -1,18 +1,19 @@
 <?php
-session_start();
-// Welbert Marques Silva                                                         
-// Web Programming II - PHP
-// Fall 2018
-// Practical Test 1
+    session_start();
+    // Welbert Marques Silva
+    // Web Programming II - PHP
+    // Fall 2018
+    // Practical Test 1
 
-if (isset($_SESSION['logged']) && $_SESSION['logged'] == 1) {
-  //Nothing if true but,
-} else {
-  $redirect = $_SERVER['PHP_SELF'];
-  header("Refresh: 3; URL=login.php?redirect=$redirect");
-  echo "You are being redirected to the login page!<br />";
-  echo "(If your browser doesn't support this, " .
-       "<a href=\"login.php?redirect=$redirect\">click here</a>)";
-  die();
-}
+    if (isset($_SESSION['logged']) && $_SESSION['logged'] == 1) {
+        //Nothing if true but,
+    } else {
+        $redirect = $_SERVER['PHP_SELF'];
+        header("Refresh: 3; URL=login.php?redirect=$redirect");
+        echo "You are being redirected to the login page!<br />";
+        echo "(If your browser doesn't support this, " .
+           "<a href=\"login.php?redirect=$redirect\">click here</a>)";
+        die();
+    }        
 ?>
+
