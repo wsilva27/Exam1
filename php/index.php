04281990441fab1ebbea158b935ea998ef35b53e
blacksmilez <43358528+blacksmilez@users.noreply.github.com>
2018-10-18 10:35:39 -0400

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,16 +1,15 @@
 <?php
-session_start();
-// Welbert Marques Silva
-// Web Programming II - PHP
-// Fall 2018
-// Practical Test 1
-
-if ((isset($_SESSION['user_logged']) &&
-      $_SESSION['user_logged'] != "") &&
-    (isset($_SESSION['user_password']) &&
-      $_SESSION['user_password'] != "")) {
-  include "logged_user.php";
-} else {
-  include "unlogged_user.php";
-}
+    session_start();
+    // Welbert Marques Silva
+    // Web Programming II - PHP
+    // Fall 2018
+    // Practical Test 1
+    if ((isset($_SESSION['user_logged']) &&
+         $_SESSION['user_logged'] != "") &&
+        (isset($_SESSION['user_password']) &&
+        $_SESSION['user_password'] != "")) {
+        include "logged_user.php";
+    } else {
+        include "unlogged_user.php";
+    }
 ?>
