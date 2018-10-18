070b2af653eafa274124e59b6f77b0905574f48e
blacksmilez <43358528+blacksmilez@users.noreply.github.com>
2018-10-18 10:40:44 -0400

    Update unlogged_user.php

--- php/unlogged_user.php
+++ php/unlogged_user.php
@@ -1,24 +1,24 @@
 <?php
-// Welbert Marques Silva
-// Web Programming - PHP
-// Fall 2018
-// Practical Test 1
+    // Welbert Marques Silva
+    // Web Programming - PHP
+    // Fall 2018
+    // Practical Test 1
 ?>
 
 <html>
 <head>
-<title>The PHP Store</title>
+    <title>The PHP Store</title>
 </head>
 <body>
-<center>
-<h1>You are currently not logged into our system.</h1>
-<p>
-  Once logged in, you will have access to your personal area<br />
-  If you have already registered,
-  <a href="user_login.php">click here</a> to login,
-  or if would like to create an account,
-  <a href="register.php">click here</a> to register.
-</p>
-</center>
+    <center>
+    <h1>You are currently not logged into our system.</h1>
+    <p>
+        Once logged in, you will have access to your personal area<br />
+        If you have already registered,         
+        <a href="user_login.php">click here</a> to login,
+        or if would like to create an account,
+        <a href="register.php">click here</a> to register.
+    </p>
+    </center>
 </body>
 </html>
