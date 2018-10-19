f0bec787b3dfca10b97bc77a8fdd377ff01ee86e
blacksmilez <43358528+blacksmilez@users.noreply.github.com>
2018-10-18 20:08:40 -0400

    Update conn_inc.php

--- php/conn_inc.php
+++ php/conn_inc.php
@@ -3,8 +3,8 @@
     // Web Programming II - PHP
     // Fall 2018
     // Practical Test 1
-    $conn = mysqli_connect('localhost:8889', 'root', 'root') or die(mysqli_error($conn));
+    $conn = mysqli_connect('localhost:3306', 'root', 'root') or die(mysqli_error($conn));
     $db = mysqli_select_db($conn, 'registration') or die(mysqli_error($conn));
-    $con = mysqli_connect("localhost", "root", "root") or die(mysqli_error($con));
+    $con = mysqli_connect("localhost:3306", "root", "root") or die(mysqli_error($con));
     $db2 = mysqli_select_db($con, "cis485test1") or die(mysqli_error($con));
 ?>
