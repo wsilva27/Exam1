ee5fb225a2052adb8d7babcb4afa53fa173819e9
blacksmilez <43358528+blacksmilez@users.noreply.github.com>
2018-10-18 20:04:56 -0400

    Update CIS485test1test1.sql

--- database/CIS485test1test1.sql
+++ database/CIS485test1test1.sql
@@ -1,8 +1,8 @@
-CREATE database CIS485test1test1;
+CREATE database CIS485test1;
 
-USE CIS485test1test1;
+USE CIS485test1;
 
-DROP TABLE IF EXISTS CIS485test1test1inventory;
+DROP TABLE IF EXISTS inventory;
 
 CREATE TABLE  inventory (
     prodid      CHAR(4)     NOT NULL,
@@ -12,15 +12,34 @@ CREATE TABLE  inventory (
     PRIMARY KEY (prodid)
 );
 
-DROP TABLE IF EXISTS ordertbl;
+-- DROP TABLE IF EXISTS ordertbl;
 
-CREATE TABLE  ordertbl (
-    ordernum INT(4)      NOT NULL AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL,
-    qty1     INT(4)      NOT NULL DEFAULT 0,
-    qty2     INT(4)      NOT NULL DEFAULT 0,
-    qty3     INT(4)      NOT NULL DEFAULT 0,
-    PRIMARY KEY (ordernum)
+-- CREATE TABLE  ordertbl (
+--     ordernum INT(4)      NOT NULL AUTO_INCREMENT,
+--     username VARCHAR(50) NOT NULL,
+--     qty1     INT(4)      NOT NULL DEFAULT 0,
+--     qty2     INT(4)      NOT NULL DEFAULT 0,
+--     qty3     INT(4)      NOT NULL DEFAULT 0,
+--     PRIMARY KEY (ordernum)
+-- );
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+	orderid int(13) unsigned NOT NULL AUTO_INCREMENT primary key,
+    username varchar(50) NOT NULL,
+    orderdate datetime
+);
+
+DROP TABLE IF EXISTS orderdetails;
+
+CREATE TABLE orderdetails (
+	detailid int(13) unsigned NOT NULL AUTO_INCREMENT primary key,
+	orderid int(13) unsigned NOT NULL,
+    prodid char(4) NOT NULL,
+    qty int(5) unsigned NOT NULL DEFAULT 0,
+    unitprice decimal(5, 2) UNSIGNED  NOT NULL, default 0,
+    FOREIGN KEY (orderid) REFERENCES orders (orderid),
+    FOREIGN KEY (prodid) REFERENCES inventory (prodid)
 );
 
 INSERT INTO inventory (prodid, proddesc, QtyOnHand, price)
@@ -29,7 +48,6 @@ INSERT INTO inventory (prodid, proddesc, QtyOnHand, price)
 VALUES ('0002','Green Gloves', 100, 20.00);
 INSERT INTO inventory (prodid, proddesc, QtyOnHand, price)
 VALUES ('0003','Red Skirt', 100, 25.00);
-
 INSERT INTO inventory (prodid, proddesc, QtyOnHand, price)
 VALUES ('0004','Blue Jacket', 100, 125.00);
 INSERT INTO inventory (prodid, proddesc, QtyOnHand, price)
