3698104d1aeb00ede273be1a950a226a4f5c7ed5
wsilva27 <welbertsilva27@gmail.com>
2018-12-10 18:51:49 -0500

    up

--- FinalTestPrep/resultbyteam.php
+++ FinalTestPrep/resultbyteam.php
@@ -5,46 +5,40 @@
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <title>Restaurants</title>
+        <title>Team</title>
     </head>
     <body>
         <div id="container">
             <?php
-            $boroin = htmlspecialchars($_GET["boro"]);
+            $teamin = htmlspecialchars($_GET["TEAM_NAME"]);
             require_once 'config.php';
             
             $conn = new mysqli($servername, $username, $password, $dbname);
-            $result = mysqli_query($conn,"call GetByBoro('".$boroin."')");
+            $result = mysqli_query($conn,"call GetByTeamName('".$teamin."')");
             $rowcount=mysqli_num_rows($result);
             
-            echo "<h3>New York City Restaurants Report: Filter By Borough " . $boroin . " (".$rowcount .")</h3>";
+            echo "<h3>United States Soccer Reports: Filter by Team " . $teamin . " (".$rowcount .")</h3>";
             
             echo "<table class='table table-bordered table-condensed'>";
             echo "    <thead>";
             echo "        <tr>";
-            echo "            <th>dba</th>";
-            echo "            <th>boro</th>";
-            echo "            <th>building</th>";
-            echo "            <th>street</th>";
-            echo "            <th>zipcode</th>";
-            echo "            <th>cuisine_description</th>";
-            echo "            <th>inspection_date</th>";
-            echo "            <th>critical_flag</th>";
-            echo "            <th>grade</th>";
+            echo "            <th>Name</th>";
+            echo "            <th>Phone</th>";
+            echo "            <th>Data of Birth</th>";
+            echo "            <th>Jersey Number</th>";
+            echo "            <th>Current Position</th>";
+            echo "            <th>Current Team</th>";
             echo "        </tr>";
             echo "    </thead>";
             echo "    <body>";
             while ($row = mysqli_fetch_array($result)){
                 echo "<tr>";
-                echo "<td>".htmlspecialchars($row['dba'])."</td>";
-                echo "<td>".htmlspecialchars($row['boro'])."</td>";
-                echo "<td>".htmlspecialchars($row['building'])."</td>";
-                echo "<td>".htmlspecialchars($row['street'])."</td>";
-                echo "<td>".htmlspecialchars($row['zipcode'])."</td>";
-                echo "<td>".htmlspecialchars($row['cuisine_description'])."</td>";
-                echo "<td>".htmlspecialchars($row['inspection_date'])."</td>";
-                echo "<td>".htmlspecialchars($row['critical_flag'])."</td>";
-                echo "<td>".htmlspecialchars($row['grade'])."</td>";
+                echo "<td>".htmlspecialchars($row['Name'])."</td>";
+                echo "<td>".htmlspecialchars($row['Phone'])."</td>";
+                echo "<td>".htmlspecialchars($row['DOB'])."</td>";
+                echo "<td>".htmlspecialchars($row['Jersey_Number'])."</td>";
+                echo "<td>".htmlspecialchars($row['Current_position'])."</td>";
+                echo "<td>".htmlspecialchars($row['Current_Team'])."</td>";
                 echo "</tr>";
             }
             echo "    </body>";
@@ -52,7 +46,8 @@
             ?>
                 
         </div>
-
-
+            <ul>
+                <li><a href="getbyteam.php">Select another Team</a></li>
+            </ul>
     </body>
 </html>
