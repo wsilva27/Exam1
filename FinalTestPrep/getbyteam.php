baef550e2b72028fbee6afeba5722a599c7b1743
wsilva27 <welbertsilva27@gmail.com>
2018-12-11 19:22:07 -0500

    changes

--- FinalTestPrep/getbyteam.php
+++ FinalTestPrep/getbyteam.php
@@ -1,40 +1,46 @@
 <?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Team Name</th><th>Goals</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
+
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+} 
 include("config.php");
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT T3.TEAM_NAME, COUNT(T1.PLAYER_ID) AS GOALS
+                            FROM PLAYER AS T1
+                                INNER JOIN SCORE AS T2 ON T1.PLAYER_ID=T2.PLAYER_ID
+                                LEFT JOIN TEAM AS T3 ON T1.TEAM_ID=T3.TEAM_ID
+                            GROUP BY T3.TEAM_NAME
+                            ORDER BY T3.TEAM_NAME;"); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+echo "<a href='index.php'>Back to Main Menu</a>"
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Team</title>
-    </head>
-    <body>
-        <h3>United States Soccer Reports: Filter by Team</h3>
-        <?php
-        include("config.php");
-        try {
-            $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            $sql = "CALL GetTeam()";
-            $q = $pdo->query($sql);
-            $q->setFetchMode(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            die("Error occurred:" . $e->getMessage());
-        }
-        ?>
-        <select onchange="loadGrades(this)">
-            <option value="">-Select Team-</option>
-            <?php while ($r = $q->fetch()): ?>
-            <option value="<?php echo $r['TEAM_NAME'] ?>"><?php echo $r['TEAM_NAME'] ?></option>
-                
-            <?php endwhile; ?>
-        </select>
-        <script>
-        function loadGrades(a){
-            team = a.value;
-            window.location = "resultbyteam.php?TEAM_NAME="+team;
-        }
-        </script>
-        <ul>
-            <li><a href="index.php">Back to main Page</a></li>
-        </ul>
-    </body>
-</html>
